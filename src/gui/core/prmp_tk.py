d7190b986b3a91ae2d2253bcb74b15d2893e38a8
prmpsmart <prmpsmart@gmail.com>
2020-10-12 00:51:26 +0100

    added a tooltip class

--- src/gui/core/prmp_tk.py
+++ src/gui/core/prmp_tk.py
@@ -1,5 +1,5 @@
 from random import randint
-import tkinter as tk, tkinter.ttk as ttk, platform
+import tkinter as tk, tkinter.ttk as ttk, platform, time
 
 TK_WIDGETS = ['Button', 'Canvas', 'Checkbutton', 'Entry', 'Frame', 'Label', 'LabelFrame', 'Listbox', 'Menu', 'Menubutton', 'Message', 'OptionMenu', 'PanedWindow', 'Radiobutton', 'Scale', 'Scrollbar', 'Spinbox', 'TKCalendar', 'TKOutput', 'Text', 'TkFixedFrame', 'TkScrollableFrame', 'Widget']
 
@@ -656,6 +656,54 @@ class ScrolledTreeView(AutoScroll, ttk.Treeview):
     def __init__(self, master, **kw):
         ttk.Treeview.__init__(self, master, **kw)
         AutoScroll.__init__(self, master)
+
+
+class ToolTip(Toplevel):
+    def __init__(self, wdgt, tooltip_font, msg=None, msgFunc=None, delay=1, follow=True):
+        self.wdgt = wdgt
+        self.parent = self.wdgt.master
+        super().__init__(self.parent, background='black', padx=1, pady=1)
+        self.withdraw()
+        self.overrideredirect(True)
+        self.msgVar = tk.StringVar()
+        if msg is None: self.msgVar.set('No message provided')
+        else: self.msgVar.set(msg)
+        self.msgFunc = msgFunc
+        self.delay = delay
+        self.follow = follow
+        self.visible = 0
+        self.lastMotion = 0
+        tk.Message(self, textvariable=self.msgVar, background='yellow', font=tooltip_font, aspect=1000).grid()
+        self.wdgt.bind('<Enter>', self.spawn, '+')
+        self.wdgt.bind('<Leave>', self.hide, '+')
+        self.wdgt.bind('<Motion>', self.move, '+')
+
+    def spawn(self, event=None):
+        self.visible = 1
+        self.after(int(self.delay * 1000), self.show)
+
+    def show(self):
+        if self.visible == 1 and time.time() - self.lastMotion > self.delay: self.visible = 2
+        if self.visible == 2: self.deiconify()
+
+    def move(self, event):
+        self.lastMotion = time.time()
+        if self.follow is False:
+            self.withdraw()
+            self.visible = 1
+        self.geometry('+%i+%i' % (event.x_root+20, event.y_root-10))
+        
+        #To get the present event coordinates
+        print(event.x_root,event.y_root)
+        
+        try: self.msgVar.set(self.msgFunc())
+        except: pass
+        self.after(int(self.delay * 1000), self.show)
+
+    def hide(self, event=None):
+        self.visible = 0
+        self.withdraw()
+
 
 
 class TwoWidgets(LabelFrame):
