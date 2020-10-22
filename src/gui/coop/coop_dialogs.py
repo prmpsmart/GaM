7ca2059dbb0911e4f5c4d7708db0dd7aee89c6bb
prmpsmart <prmpsmart@gmail.com>
2020-10-22 20:35:14 +0100

    added coop gui

---
+++ src/gui/coop/coop_dialogs.py
@@ -0,0 +1,12 @@
+from ..core.dialogs import PersonDialog
+
+class NewClientDialog(PersonDialog):
+    
+    def __init__(self, master=None, title='New Client Dialog', **kwargs):
+        super().__init__(master=master, title=title, **kwargs)
+
+
+
+
+
+
