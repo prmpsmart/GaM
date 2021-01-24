5e0ebc61a0149de5bc8763295766eac886957e4b
prmpsmart <prmpsmart@gmail.com>
2021-01-24 18:53:25 +0100

    modifing the auths.py

--- src/utils/auths.py
+++ src/utils/auths.py
@@ -1,5 +1,5 @@
 import hashlib, zlib, base64
-
+from prmp_miscs.prmp_mixins import PRMP_Mixins
 
 class Auths_Vars:
     
@@ -34,89 +34,140 @@ class Auths_Vars:
     def not_exist(cls, usr): return "User %s doesn't exist"%usr
     
     @classmethod
-    def get_admin_text(cls): return cls.__decompit(cls.admin_text).decode()
+    def get_admin_text(cls):
+        decomp = cls._decompit(cls.admin_text)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_non_admin_text(cls): return cls.__decompit(cls.non_admin_text).decode()
+    def get_non_admin_text(cls):
+        decomp = cls._decompit(cls.non_admin_text)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_empty(cls): return cls.__decompit(cls.non_admin_text).decode()
+    def get_empty(cls):
+        decomp = cls._decompit(cls.non_empty)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_wrong_pass(cls): return cls.__decompit(cls.wrong_pass).decode()
+    def get_wrong_pass(cls):
+        decomp = cls._decompit(cls.wrong_pass)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_no_user(cls): return cls.__decompit(cls.no_user).decode()
+    def get_no_user(cls):
+        decomp = cls._decompit(cls.no_user)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_insuffcient(cls): return cls.__decompit(cls.insuffcient).decode()
+    def get_insuffcient(cls):
+        decomp = cls._decompit(cls.insuffcient)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_added(cls): return cls.__decompit(cls.added).decode()
+    def get_added(cls):
+        decomp = cls._decompit(cls.added)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_pass_changed(cls): return cls.__decompit(cls.pass_changed).decode()
+    def get_pass_changed(cls):
+        decomp = cls._decompit(cls.pass_changed)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_user_changed(cls): return cls.__decompit(cls.user_changed).decode()
+    def get_user_changed(cls):
+        decomp = cls._decompit(cls.user_changed)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_cant_super(cls): return cls.__decompit(cls.del_super).decode()
+    def get_cant_super(cls):
+        decomp = cls._decompit(cls.del_super)
+        dec = decomp.decode()
+        return dec
     @classmethod
-    def get_login_successful(cls): return cls.__decompit(cls.login_successful).decode()
+    def get_login_successful(cls):
+        decomp = cls._decompit(cls.login_successful)
+        dec = decomp.decode()
+        return dec
 
-    
     @classmethod
-    def __hashit(cls, text):
+    def _hashit(cls, text):
         try: text = text.encode()
         except: pass
-        return hashlib.sha224(base64.b64encode(zlib.compress(text))).hexdigest()
+        comp = zlib.compress(text)
+        enc = base64.b64encode(comp)
+        sha = hashlib.sha224(enc)
+        hexdigest = sha.hexdigest()
+        return hexdigest
     
     @classmethod
-    def __compit(cls, text):
+    def _compit(cls, text):
         try: text = text.encode()
         except: pass
-        return base64.b64encode(zlib.compress(text))
+        comp = zlib.compress(text)
+        enc = base64.b64encode(comp)
+        return enc
     
     @classmethod
-    def __decompit(cls, text):
+    def _decompit(cls, text):
         try: text = text.encode()
         except: pass
-        return zlib.decompress(base64.b64decode(text))
+        dec = base64.b64decode(text)
+        decomp = zlib.decompress(dec)
+        return decomp
 
 
-class User:
+class User(PRMP_Mixins):
     super_user = False
-    class_name = "User"
+    ADMIN = False
 
-    def __init__(self, name, usr, pwd, hint, admin):
-        self.name = name
-        self.username = usr
-        self.password = pwd
-        self.hint = hint
-        self.permission = admin
+    def __init__(self, name, usr, pwd, hint, permission):
+        self.__name = name
+        self.__username = usr
+        self.__password = pwd
+        self.__hint = hint
+        self.__permission = permission
     
-    def __repr__(self):
-        text = f"{self.class_name}(name={self.get_name()}, username={self.get_username()}, password={self.get_password()}, hint={self.get_hint()}, permission={self.get_permission()})"
-        return text
+    def __repr__(self): return f'<{self.username}, {self.permission}>'
     
     def __str__(self):
-        text = f"{self.class_name}(name={self.get_name()}, username={self.get_username()}, password={self.get_password()}, hint={self.get_hint()}, permission={self.get_permission()})"
+        text = f"{self.className}(name={self.name}, username={self.username}, password={self.password}, hint={self.hint}, permission={self.permission})"
         return text
     
-    def get_status(self): return (self.get_name(), self.get_username())
-
-    def get_name(self): return Auths_Vars._Auths_Vars__decompit(self.name).decode()
-    def get_username(self): return Auths_Vars._Auths_Vars__decompit(self.username).decode()
+    @property
+    def name(self): return Auths_Vars._decompit(self.__name).decode()
+    @property
+    def username(self): return Auths_Vars._decompit(self.__username).decode()
     
-    def get_name_lower(self): return self.get_name().lower()
-    def get_username_lower(self): return self.get_username().lower()
-    
-    def get_unhash(self):
-        vals = [self.get_name(), self.get_username(), self.get_password(), self.get_hint(), self.get_permission()]
-        return vals
-    
-    def get_password(self): return Auths_Vars._Auths_Vars__decompit(self.password).decode()
-    def get_hint(self): 
-        hint = Auths_Vars._Auths_Vars__decompit(self.hint).decode()
+    @property
+    def name_lower(self): return self.name.lower()
+    @property
+    def username_lower(self): return self.username.lower()
+    @property
+    def password(self): return self.__password
+    @property
+    def _password(self): return Auths_Vars._decompit(self.__password).decode()
+    @property
+    def hint(self): 
+        hint = Auths_Vars._decompit(self.__hint).decode()
         if not hint: return "EMPTY"
         return hint
-    def get_permission(self): return Auths_Vars.get_permission(self.permission)[1]
+    @property
+    def permission(self): return Auths_Vars.get_permission(self.__permission)[1]
     
-    def is_admin(self): return Auths_Vars.get_permission(self.permission)[0]
+    @property
+    def status(self): return (self.name, self.username)
+
+    @property
+    def unhash(self):
+        vals = [self.name, self.username, self.password, self.hint, self.permission]
+        return vals
     
-    def get_hash(self):
+    
+    @property
+    def is_admin(self): return Auths_Vars.permission(self.permission)[0]
+    
+    @property
+    def _hash(self):
         vals = [self.name, self.username, self.password, self.hint, self.permission]
         return vals
 
@@ -124,24 +175,21 @@ class User:
 class Super_User(User):
     super_user = True
     ADMIN = True
-    class_name = "Super_User"
-    def __init__(self, *args):
-        super().__init__(*args) 
+
 
 
 class Authorisation(Auths_Vars):
-    
     current_user = None
-
-    users = []
-    # boss1 = Super_User(b'eJwL8nf2jlTw9QxydI5UCHANcQ0CADAnBQ4=', b'eJwrKMotKM5NLCoBABOSA+c=', b'eJwrKMrMS04tygUAD04DYQ==', b'eJzLyy/KTcwBAAkCAoo=', cls.admin)
-    # boss2 = Super_User(b'eJzz9/EPCvUL8XfyVHBydQwJ8nR2VXB0cXUJ9fN2BQBznQfO', b'eJxLTElKTSzJzwYADjoDPA==', b'eJxLSk0sSSnNywYADpsDTw==', b'eJzLS1RIKc3LTgUADSwDBw==', cls.admin)
-    # super_users = [boss1, boss2]
-    super_users = [
+    __users = []
+    __super_users = [
         Super_User(b'eJwL8nf2jlTw9QxydI5UCHANcQ0CADAnBQ4=', b'eJwrKMotKM5NLCoBABOSA+c=', b'eJwrKMrMS04tygUAD04DYQ==', b'eJzLyy/KTcwBAAkCAoo=', Auths_Vars.admin), 
         Super_User(b'eJzz9/EPCvUL8XfyVHBydQwJ8nR2VXB0cXUJ9fN2BQBznQfO', b'eJxLTElKTSzJzwYADjoDPA==', b'eJxLSk0sSSnNywYADpsDTw==', b'eJzLS1RIKc3LTgUADSwDBw==',  Auths_Vars.admin)
         ]
     
+    @classmethod
+    def get_users(cls): return cls.__users
+    @classmethod
+    def get_super_users(cls): return cls.__super_users
     @classmethod
     def check_super(cls, usr, pwd):
         user = cls.get_user(usr, pwd)
@@ -167,62 +215,62 @@ class Authorisation(Auths_Vars):
     def check_name(cls, name): return name.strip().lower() in cls.get_names_lower()
     
     @classmethod
-    def check_super_users(cls, usr): return cls._Auths_Vars__compit(usr) in [user[0] for user in cls.super_users]
+    def check_super_users(cls, usr): return cls._compit(usr) in [user[0] for user in cls.__super_users]
     
 
     
     @classmethod
     def add_user(cls, name, usr, pwd, hint='', admin=False):
-        if (10 <= len(usr) <= 20) and (10 <= len(pwd) <= 20): 
+        if (5 <= len(usr) <= 20) and (10 <= len(pwd) <= 20): 
             if name and usr and pwd:
                 name = name.strip()
                 usr = usr.strip()
                 if cls.check_name(name): return "Name already exists"
                 elif cls.check_username(usr): return "Username already exists"
                 else:
-                    new = User(cls._Auths_Vars__compit(name), cls._Auths_Vars__compit(usr), cls._Auths_Vars__compit(pwd), cls._Auths_Vars__compit(hint), cls.admin if admin else cls.non_admin)
-                    cls.users.append(new)
-                    return cls.added
-        else: return cls._Auths_Vars__decompit(cls.insufficient).decode()
+                    new = User(cls._compit(name), cls._compit(usr), cls._compit(pwd), cls._compit(hint), cls.admin if admin else cls.non_admin)
+                    cls.__users.append(new)
+                    return cls._decompit(cls.added).decode(), new
+        else: return cls._decompit(cls.insufficient).decode()
     
     @classmethod
     def get_usernames(cls):
-        users = [*[user.get_username() for user in cls.users], *[user.get_username() for user in cls.super_users]]
+        users = [*[user.username for user in cls.__users], *[user.username for user in cls.__super_users]]
         return users
     
     @classmethod
     def get_usernames_lower(cls):
-        users = [*[user.get_username_lower() for user in cls.users], *[sup_user.get_username_lower() for sup_user in cls.super_users]]
+        users = [*[user.username_lower for user in cls.__users], *[sup_user.username_lower for sup_user in cls.__super_users]]
         return users
     
     @classmethod
-    def get_names(cls): return [*[user.get_name() for user in cls.users], *[user.get_name() for user in cls.super_users]]
+    def get_names(cls): return [*[user.name for user in cls.__users], *[user.name for user in cls.__super_users]]
     
     @classmethod
-    def get_names_lower(cls): return [*[user.get_name_lower() for user in cls.users], *[user.name for user in cls.super_users]]
+    def get_names_lower(cls): return [*[user.name_lower for user in cls.__users], *[user.name for user in cls.__super_users]]
     
     @classmethod
-    def get_users(cls): return [*cls.users, *cls.super_users]
+    def get_users(cls): return [*cls.__users, *cls.__super_users]
         
     @classmethod
     def get_username(cls, h_usr):
         for usr in cls.get_usernames():
-            if h_usr == cls._Auths_Vars__compit(usr): return usr
+            if h_usr == cls._compit(usr): return usr
         
     @classmethod
     def get_user(cls, usr, pwd):
         all_ = cls.get_users()
         for user in all_:
-            if cls._Auths_Vars__compit(usr) == user.username:
-                if cls._Auths_Vars__compit(pwd) == user.password: return user
+            if cls._compit(usr) == user.username:
+                if cls._compit(pwd) == user.password: return user
                 else: return cls.wrong_pass
         return cls.not_exist(usr)
     
     @classmethod
-    def __get_user_unhash(cls, usr, pwd):
+    def _get_user_unhash(cls, usr, pwd):
         user = cls.get_user(usr, pwd)
         if user != cls.wrong_pass and user != cls.not_exist(usr):
-            au = [user.get_unhash()]
+            au = [user.unhash]
             au[-1] = cls.get_permission(au[-1])
             return au
         return user
@@ -232,26 +280,26 @@ class Authorisation(Auths_Vars):
         if cls.check_username(usr) == 1:
             users = cls.get_users()
             for user in users:
-                if usr == user.get_username(): return user.get_hint()
+                if usr == user.username: return user.hint
     
     @classmethod
     def delete_user(cls, usr, pwd):
         if usr:
             if cls.check_super_users(usr): return cls.get_cant_super()
             elif cls.check_username(usr):
-                for user in cls.users:
-                    if user.get_username() == usr and user.get_.password() == pwd:
-                        cls.users.remove(user)
+                for user in cls.__users:
+                    if user.username == usr and user.password == pwd:
+                        cls.__users.remove(user)
                         del user
             else:  return "User %s doesn't exists"%usr
     @classmethod
     def change_password(cls, usr, old_pwd, new_pwd, hint=''):
         if usr and old_pwd and new_pwd:
             if cls.check_username(usr):
-                h_usr = cls._Auths_Vars__compit(usr)
-                h_old_pwd = cls._Auths_Vars__compit(old_pwd)
-                h_new_pwd = cls._Auths_Vars__compit(new_pwd)
-                for user in cls.users:
+                h_usr = cls._compit(usr)
+                h_old_pwd = cls._compit(old_pwd)
+                h_new_pwd = cls._compit(new_pwd)
+                for user in cls.__users:
                     if (user.username == h_usr) and (user.password == h_old_pwd):
                         user.password = h_new_pwd
                         return cls.pass_changed
@@ -261,38 +309,38 @@ class Authorisation(Auths_Vars):
         if old_usr and new_usr and pwd:
             if cls.check_username(new_usr): return "Username already exists"
             else:
-                h_old_usr = cls._Auths_Vars__compit(old_usr)
-                h_new_usr = cls._Auths_Vars__compit(new_usr)
-                h_pwd = cls._Auths_Vars__compit(pwd)
-                for user in cls.users:
+                h_old_usr = cls._compit(old_usr)
+                h_new_usr = cls._compit(new_usr)
+                h_pwd = cls._compit(pwd)
+                for user in cls.__users:
                     if (user.username == h_old_usr) and (user.password == h_pwd): user.username = h_new_usr
                 return cls.user_changed
     @classmethod
     def make_admin(cls, usr='', pwd=''):
         if usr and pwd:
-            h_usr = cls._Auths_Vars__compit(usr)
-            h_pwd = cls._Auths_Vars__compit(pwd)
-            for user in cls.users:
+            h_usr = cls._compit(usr)
+            h_pwd = cls._compit(pwd)
+            for user in cls.__users:
                 if (user[1] == h_usr) and (user[2] == h_pwd): user[-1] = cls.admin
     @classmethod
     def make_non_admin(cls, usr='', pwd=''):
         if usr and pwd:
-            h_usr = cls._Auths_Vars__compit(usr)
-            h_pwd = cls._Auths_Vars__compit(pwd)
-            for user in cls.users:
+            h_usr = cls._compit(usr)
+            h_pwd = cls._compit(pwd)
+            for user in cls.__users:
                 if (user[1] == h_usr) and (user[2] == h_pwd): user[-1] = cls.non_admin
     
     @classmethod
     def load_users(cls, users):
         for user_ in users:
-            if user_[0] in [user[0] for user in cls.users]: continue
-            else: cls.users.append(User(*user_))
+            if user_[0] in [user[0] for user in cls.__users]: continue
+            else: cls.__users.append(User(*user_))
     
     @classmethod
     def load_super_users(cls, users):
         users_ = []
         for user in users: users_.append(User(*user))
-        cls.super_users = users_
+        cls.__super_users = users_
 
     @classmethod
     def login(cls, usr, pwd):
@@ -319,7 +367,7 @@ class Authorisation(Auths_Vars):
         return cls.get_no_user()
     @classmethod
     def get_current_user_permission(cls):
-        if cls.current_user: return cls.current_user.get_permission()
+        if cls.current_user: return cls.current_user.permission
         return cls.get_non_admin_text()
     @classmethod
     def logged_in(cls): return cls.current_user != None
@@ -333,7 +381,7 @@ class Authorisation(Auths_Vars):
     @classmethod
     def __all_users_unhash(cls):
         users = []
-        for user in cls.all_users(): users.append(user.get_unhash())
+        for user in cls.all_users(): users.append(user.unhash)
         return users
     
     @classmethod
