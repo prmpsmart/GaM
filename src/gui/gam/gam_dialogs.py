aaa19352761882a2bff1bff92689523081e43b61
Apata Miracle Peter <prmpsmart@gmail.com>
2021-03-21 13:27:35 +0100

    corrected

--- src/gui/gam/gam_dialogs.py
+++ src/gui/gam/gam_dialogs.py
@@ -42,13 +42,13 @@ class PersonDialog(GaM_Dialog):
 
         self.contact = self.addWidget(PRMP_Style_LabelFrame, config=dict(config=dict(text='Contact Details')), place=dict(x=2, y=2, h=250, relw=.55))
 
-        self.name = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Name')), bottomKwargs=dict(required=True), orient='h', place=dict(relx=.02, rely=0, relh=.15, relw=.96), longent=.25)
+        self.name = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Name')), bottomKwargs=dict(required=True), orient='h', place=dict(relx=.02, rely=0, relh=.14, relw=.96), longent=.25)
 
-        self.phone = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Phone Number')), bottomKwargs=dict(_type='number'), place=dict(relx=.02, rely=.14, relh=.15, relw=.96), longent=.5, orient='h')
+        self.phone = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Phone Number')), bottomKwargs=dict(_type='number'), place=dict(relx=.02, rely=.14, relh=.14, relw=.96), longent=.5, orient='h')
 
-        self.email = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Email')), bottomKwargs=dict(_type='email'), place=dict(relx=.02, rely=.28, relh=.15, relw=.96), longent=.25, orient='h')
+        self.email = LabelEntry(self.contact,  topKwargs=dict(config=dict(text='Email')), bottomKwargs=dict(_type='email'), place=dict(relx=.02, rely=.28, relh=.14, relw=.96), longent=.25, orient='h')
 
-        self.gender = LabelCombo(self.contact,  topKwargs=dict(config=dict(text='Gender')), bottomKwargs=dict(type_='gender'), place=dict(relx=.02, rely=.42, relh=.15, relw=.96), longent=.25, orient='h')
+        self.gender = LabelCombo(self.contact,  topKwargs=dict(config=dict(text='Gender')), bottomKwargs=dict(type_='gender'), place=dict(relx=.02, rely=.42, relh=.14, relw=.96), longent=.25, orient='h')
 
         self.address = LabelText(self.contact,  topKwargs=dict(config=dict(text='Address')), place=dict(relx=.02, rely=.56, relh=.44, relw=.96), bottomKwargs=dict(required=True), longent=.3, orient='h')
 
